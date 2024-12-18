435fd3097792feaed6e5acbd9e21dfcc17ff6448
slava0135 <super.novalskiy_0135@inbox.ru>
2024-12-18 20:50:05 +0300

    a

--- stg.c
+++ stg.c
@@ -1,7 +1,7 @@
 #include "stdio.h"
 #include "stdlib.h"
 
-#define PRINT_NAME() printf("[%s]\n", __func__)
+#define PRINT_FUNCTION_NAME() printf("[%s]\n", __func__)
 #define JUMP(code_label) return (CodeLabel) & code_label
 #define ENTER(node) JUMP((*((CodeLabel **)node[0]))[0])
 
@@ -11,6 +11,8 @@ typedef void *StgWord;
 StgWord Sp[10000];
 StgWord *SpB = Sp;
 StgWord **SpA = (StgWord **)&Sp[10000];
+
+StgWord *Node;
 
 void push_b(void *value) {
   SpB[0] = (StgWord)value;
@@ -32,12 +34,42 @@ StgWord *pop_a() {
   return SpB[-1];
 }
 
+///// plus /////
+
+CodeLabel plus_return_Int2() {
+  PRINT_FUNCTION_NAME();
+  ENTER(Node);
+}
+StgWord plus_return_vec2[] = {plus_return_Int2};
+
+CodeLabel plus_return_Int1() {
+  PRINT_FUNCTION_NAME();
+  ENTER(Node);
+}
+StgWord plus_return_vec1[] = {plus_return_Int1};
+
+/* plus = {} \n {l,r} -> ... */
+CodeLabel plus_direct() {
+  PRINT_FUNCTION_NAME();
+  push_b(plus_return_vec1); // push case alternatives on return stack
+  Node = SpA[0];
+  ENTER(Node);
+}
+CodeLabel plus_entry() {
+  PRINT_FUNCTION_NAME();
+  JUMP(plus_direct);
+}
+StgWord plus_info[] = {plus_entry};
+StgWord plus_closure[] = {&plus_info};
+
+///// main /////
+
 CodeLabel main_direct() {
-  PRINT_NAME();
+  PRINT_FUNCTION_NAME();
   exit(0);
 }
 CodeLabel main_entry() {
-  PRINT_NAME();
+  PRINT_FUNCTION_NAME();
   JUMP(main_direct);
 }
 StgWord main_info[] = {main_entry};
